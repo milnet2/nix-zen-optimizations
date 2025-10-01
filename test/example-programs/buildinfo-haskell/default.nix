b7953a2d3468fb7a2d5df588dbe2da201c35695e
Tobias Blaschke <code@tobiasblaschke.de>
2025-10-01 11:03:31 +0200

    Tweak GHC test program

--- test/example-programs/buildinfo-haskell/default.nix
+++ test/example-programs/buildinfo-haskell/default.nix
@@ -8,13 +8,7 @@ stdenv.mkDerivation {
   nativeBuildInputs = [ ghc ];
 
   buildPhase = ''
-    ${ghc}/bin/ghc -O2 -cpp \
-      -optP-D__FAST_MATH__ \
-      -optP-D__x86_64__=1 \
-      -optP-D__SSE__ -optP-D__SSE2__ -optP-D__SSE3__ -optP-D__SSSE3__ -optP-D__SSE4_1__ -optP-D__SSE4_2__ \
-      -optP-D__AVX__ -optP-D__AVX2__ \
-      Main.hs -o buildinfo
-    echo "${ghc}/bin/ghc -O2 -cpp -optP-D__FAST_MATH__ -optP-D__x86_64__=1 -optP-D__SSE__ -optP-D__SSE2__ -optP-D__SSE3__ -optP-D__SSSE3__ -optP-D__SSE4_1__ -optP-D__SSE4_2__ -optP-D__AVX__ -optP-D__AVX2__ Main.hs -o buildinfo" >buildinfo.log || true
+    ${ghc}/bin/ghc Main.hs -o buildinfo
     ./buildinfo >buildinfo.json
   '';
 
