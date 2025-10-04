2031c3d2b73f02ad6fc3d4071414cf263cd941eb
Tobias Blaschke <code@tobiasblaschke.de>
2025-10-04 12:36:34 +0200

    Refactor: Move library overlays

--- zen-optimized-pkgs.nix
+++ zen-optimized-pkgs.nix
@@ -200,75 +200,7 @@ let
     # ---------------------------------------------
     # Overrides follow (libraries)
 
-# See: docu/blas-implementations.adoc
-# tag::openBlasOverlay[]
-    openBlasOverlay = (final: prev: rec {
-        aocl-utils = prev.aocl-utils.override {
-            # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/ao/aocl-utils/package.nix
-            # TODO: We'd likely want fast-math here even if isAggressiveFastMathEnabled is disabled
-        };
-
-        amd-blis = prev.amd-blis.override {
-            # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/am/amd-blis/package.nix#L70
-            # TODO: We'd likely want fast-math here even if isAggressiveFastMathEnabled is disabled
-            inherit (noOptimizePkgs) perl; # TODO: Python
-            blas64 = false; # TODO: check
-            withOpenMP = true; # TODO: check
-            withArchitecture = "zen${toString amdZenVersion}";
-            inherit (unoptimizedPkgs) stdenv; # TODO Because of LTO
-        };
-
-        amd-libflame = prev.amd-libflame.override {
-            # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/am/amd-libflame/package.nix
-            # TODO: We'd likely want fast-math here even if isAggressiveFastMathEnabled is disabled
-            inherit amd-blis aocl-utils;
-            inherit (noOptimizePkgs) cmake;
-            inherit (final) gfortran;
-            blas64 = false; # TODO: check
-            withOpenMP = true; # TODO: check
-            withAMDOpt = true;
-        };
-
-        # https://search.nixos.org/packages?channel=unstable&show=openblas&query=openblas
-        openblas = prev.openblas.override {
-          # See https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/development/libraries/science/math/openblas/default.nix
-          # TODO: We'd likely want fast-math here even if isAggressiveFastMathEnabled is disabled
-          enableAVX512 = optimizedPlatform.isAvx512; # TODO: These kernels have been a source of trouble in the past.
-          openmp = true;
-          # See https://github.com/OpenMathLib/OpenBLAS/blob/develop/TargetList.txt
-          target = "ZEN";
-          dynamicArch = false;  # prefer fixed-target
-        };
-
-        lapack-reference = prev.lapack-reference # AKA liblapack
-            .override {
-                # https://search.nixos.org/packages?channel=25.05&show=lapack-reference&query=liblapack
-                # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/la/lapack-reference/package.nix
-                # TODO: We'd likely want fast-math here even if isAggressiveFastMathEnabled is disabled
-                inherit (final) gfortran;
-                inherit (noOptimizePkgs) cmake;
-                inherit (unoptimizedPkgs) stdenv; # TODO Because of LTO
-            };
-
-        blas = prev.blas.override {
-            # https://search.nixos.org/packages?channel=unstable&show=blas&query=blas
-            # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/bl/blas/package.nix
-            # TODO: We'd likely want fast-math here even if isAggressiveFastMathEnabled is disabled
-            inherit openblas lapack-reference;
-            blasProvider = final.amd-blis;
-        };
-
-        # See also: la-pack https://github.com/ROCm/rocm-libraries
-
-        lapack = prev.lapack.override {
-            # https://search.nixos.org/packages?channel=unstable&show=lapack&query=lapack
-            # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/la/lapack/package.nix
-            # TODO: We'd likely want fast-math here even if isAggressiveFastMathEnabled is disabled
-            inherit openblas lapack-reference;
-            lapackProvider = final.amd-libflame;
-        };
-    });
-# end::openBlasOverlay[]
+    openBlasOverlay = import ./overlays/library/blas-lapack/default.nix { inherit optimizedPlatform unoptimizedPkgs amdZenVersion; };
 
     # TODO: OpenMP
 
